f7f008d11bea16be8b531ee0bce8ef793c32ce96
wangli147 <1475986800@qq.com>
2017-08-23 20:35:14 +0800

    更新

---
+++ routes/parcel.js
@@ -0,0 +1,29 @@
+var express=require("express");
+var mysql=require("mysql");
+var router=express.Router();
+
+var pool=mysql.createPool({
+	host:"localhost",// 127.0.0.1
+	user:"root",//用户名
+	password:"",//密码
+	database:"livehere",//数据库
+	port:"3306"
+});
+
+router.post("/par",function(req,res){
+	// var id=req.body["id"];
+	var name=req.body["username"];
+	var type=req.body["messtype"];
+	var code=req.body["code"];
+	var phone=req.body["phone"];
+	var address=req.body["address"];
+
+	res.header("Access-Control-Allow-Origin", "*");
+		pool.query(`insert into parcel (username) values ('${name}')`,function(err,rows,fields){
+		if(err) throw err;
+		res.send(rows);
+	});
+
+});
+module.exports=router;
+
