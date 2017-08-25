c8f5ed89c4b87c72c88896a01649c123c1a5de04
sugerss <1915492618@qq.com>
2017-08-25 08:46:34 +0800

    cc 08:46

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var ownerinfo = require('./routes/ownerinfo');
-
+var notice = require('./routes/notice');
 var app = express();
 
 
@@ -27,9 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
 app.use('/ownerinfo',ownerinfo);
-
+app.use('/notice',notice);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
