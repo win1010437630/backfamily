6706d5378c0da865916c3811242abf058a3a13f8
xiuzhenqing <741337605@qq.com>
2017-09-05 09:49:00 -0500

    修---关于维修的后台

--- routes/service.js
+++ routes/service.js
@@ -10,13 +10,26 @@ var pool=mysql.createPool({
 	database:'livehere',
 	port:3306
 });
-
+/*维修获取数据库*/
 router.get("/service",function(req,res){
 	res.header("Access-Control-Allow-Origin", "*");
 		pool.query('select * from service',function(err,rows,fields){
 			if(err) throw err;
 			res.send(rows)
 	
+		})
+})
+/*维修提交新数据，以及重新获取获取*/
+router.post("/ser",function(req,res){
+	var user=req.body["user"];
+	var content=req.body["content"];
+	res.header("Access-Control-Allow-Origin", "*");
+		pool.query(`insert into parcel (user,content) values ('${user}','${content}')`,function(err,rows,fields){
+			if(err) throw err;
+			if(rows){
+				res.send('success')
+			}
+
 		})
 })
 module.exports=router;
