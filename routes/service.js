547cde265475f6facbe0683ac2f1fd918d37929a
xiuzhenqing <741337605@qq.com>
2017-09-05 09:54:01 -0500

    修--完善维修后台数据接口

--- routes/service.js
+++ routes/service.js
@@ -12,23 +12,33 @@ var pool=mysql.createPool({
 });
 /*维修获取数据库*/
 router.get("/service",function(req,res){
+	var order=[];
 	res.header("Access-Control-Allow-Origin", "*");
 		pool.query('select * from service',function(err,rows,fields){
 			if(err) throw err;
-			res.send(rows)
-	
+			for(var i in rows){
+				order.unshift(rows[i])
+			}
+			res.send(order)	
 		})
 })
 /*维修提交新数据，以及重新获取获取*/
 router.post("/ser",function(req,res){
 	var user=req.body["user"];
 	var content=req.body["content"];
+	var order=[];
 	res.header("Access-Control-Allow-Origin", "*");
 		pool.query(`insert into parcel (user,content) values ('${user}','${content}')`,function(err,rows,fields){
+			pool.query('select * from service',function(err,rows,fields){
 			if(err) throw err;
-			if(rows){
-				res.send('success')
+			for(var i in rows){
+				order.unshift(rows[i])
 			}
+			res.send(order)
+	
+		})
+
+
 
 		})
 })
